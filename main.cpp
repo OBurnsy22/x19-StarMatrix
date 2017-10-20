ea2bad32fc3154a313a139029a73e512e230f338
OBurnsy22 <mburns22@mail.csuchico.edu>
2017-10-20 10:39:32 -0700

    Asks user for rows and columns of stars and outupts what the user put in.

---
+++ main.cpp
@@ -0,0 +1,39 @@
+//Author: Matthew Burns
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main() {
+
+int cols, rows;
+vector <vector<char> > starMatrix;
+
+cout<<"Enter rowns and columns of stars:"<<endl;
+cin>>rows;
+cin>>cols;
+
+starMatrix.resize(rows);
+
+if(rows > 0 && cols > 0)
+{
+    for(int r = 0; r < rows; r++)
+    {
+        starMatrix[r].resize(cols);
+    }
+    
+    for(int x = 0; x < starMatrix.size(); x++)
+    {
+        for(int y =0; y < starMatrix[x].size(); y++)
+        {
+            starMatrix[x][y] = '*';
+            cout<<starMatrix[x][y];
+        }
+        cout<<endl;
+    }    
+
+}
+    
+return 0;
+}
